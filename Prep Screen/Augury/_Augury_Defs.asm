87fdbef18e985d967c66e8ca34ea57988ab7ecd3
LNB2017 <33029766+LNB2017@users.noreply.github.com>
2019-08-04 09:58:10 -0400

    updated mockup, made defs prettier

--- Prep Screen/Augury/_Augury_Defs.asm
+++ Prep Screen/Augury/_Augury_Defs.asm
@@ -1,30 +1,28 @@
 .thumb
 .include "../_Prep_Screen_Defs.asm"
 
-.equ InitialTextStruct, 0x200E864	@used by the "main" prep screen
+.equ InitialTextStruct, 	0x200E864	@used by the "main" prep screen
 
-.equ Proc_InitialOffset, 0x2C		@all the rest of this stuff begins here
-
-.equ max_chapter_id, 89
+.equ Proc_InitialOffset, 	0x2C		@all the rest of this stuff begins here
 @ bytes - 8 bits
-.equ Proc_RowNumber, 0x00
-.equ Proc_HandY, 0x01
-.equ Proc_CombatRatio, 0x02
-.equ Proc_Losses, 0x03
-.equ Proc_LatestChapter, 0x04
-.equ Proc_DisplayChapter, 0x05
-
+.equ Proc_RowNumber, 		0x00
+.equ Proc_HandY, 			0x01
+.equ Proc_CombatRatio, 		0x02
+.equ Proc_Losses, 			0x03
+.equ Proc_LatestChapter, 	0x04
+.equ Proc_DisplayChapter, 	0x05
 @ shorts - 16 bits
-.equ Proc_TurnCount, 0x0C
-.equ Proc_TotalLevels, 0x0E
-
+.equ Proc_TurnCount, 		0x0C
+.equ Proc_TotalLevels, 		0x0E
 @ words - 32 bits
-.equ Proc_TotalExp, 0x18
-.equ Proc_TotalFunds, 0x1C
+.equ Proc_TotalExp, 		0x18
+.equ Proc_TotalFunds, 		0x1C
 
-.equ Tactics, 0
-.equ Combat, 1
-.equ Survival, 2
-.equ Experience, 3
-.equ Power, 4
-.equ Funds, 5
+.equ Tactics, 				0
+.equ Combat, 				1
+.equ Survival, 				2
+.equ Experience, 			3
+.equ Power, 				4
+.equ Funds, 				5
+
+.equ max_chapter_id, 		89
