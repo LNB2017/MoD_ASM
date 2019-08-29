e7218bd5c03dd1b58212fed8f0be2404b9efd0b1
BM <blackwizorbe@gmail.com>
2019-08-29 00:39:28 -0300

    Added initializations to Set_Up_Augury_Proc.asm

--- Prep Screen/Augury/Set_Up_Augury_Proc.asm
+++ Prep Screen/Augury/Set_Up_Augury_Proc.asm
@@ -2,6 +2,7 @@
 .include "_Augury_Defs.asm"
 
 .equ Augury_Palette, Augury_Graphics+4
+.equ Is_Base_Map, Augury_Palette+4
 
 @r0=proc pointer
 push    {r4-r7,r14}
@@ -9,10 +10,9 @@ mov     r4,r0
 mov     r7,r4
 add     r4,#Proc_InitialOffset
 
-@clearing bglayer3
-ldr     r0,=BGLayer3
-mov     r1,#0
-_blh    ClearLayer
+@clear all 
+mov     r0,#0
+_blh    #0x80026BC
 
 @red background graphics
 ldr     r0,=#0x8336D5C       
@@ -53,10 +53,51 @@ mov     r2,#0x20    @length
 _blh    CopyToPaletteBuffer
 
 @TODO: Set up black box object transparency
+@initialization
+mov     r0,#0
+strb    r0,[r4,#Proc_RowNumber]    @current row for bottow text
 
-@red background enable
-mov     r0,#8
-_blh    EnableBGSyncByMask
+mov     r0,#(8*(RowHeader_X+2+1))
+strb    r0,[r4,#Proc_HandY]    @current y pixel position for the hand
+
+_blh    0x808F470 @function that gets the win ratio for combat rank
+strb    r0,[r4,#Proc_CombatRatio]
+
+_blh    0x808F4F0 @function that gets the number of killed units for survival rank
+strb    r0,[r4,#Proc_Losses]
+
+_blh    0x808F30C @function that gets the current turncount for tactics rank
+strh    r0,[r4,#Proc_TurnCount]
+
+_blh    0x808F648 @function that gets the sum of levels in the whole army for power rank
+strh    r0,[r4,#Proc_TotalLevels]
+
+_blh    0x8084D34 @function that gets the sum of experience in the whole army for Exp rank
+str     r0,[r4,#Proc_TotalExp]
+
+_blh    0x8017104 @function that gets the total funds (gold + sale worth of all items) for funds rank 
+str     r0,[r4,#Proc_TotalFunds]
+
+ldr     r5,=gChapterData
+ldrb    r5,[r5,#0xE] @loading current chapter  
+chapterLoop:
+sub     r5, #1
+mov     r0, r5
+ldr     r3,Is_Base_Map
+_blr    r3   @returns 0 if not a base, 1 if is a base
+cmp     r0, #0
+beq     chapterLoopEnd @not a base, so we'll store this chapter
+cmp     r5, #0 @we want to avoid an underflow here
+bgt     chapterLoop
+chapterLoopEnd:
+strb    r5,[r4,#Proc_LatestChapter]
+strb    r5,[r4,#Proc_DisplayChapter]
+
+@initialize font
+mov        r0,#0
+_blh    Font_InitDefault
+
+
 
 pop     {r4-r7}
 pop     {r1}
